bc96d442e2f13d62e383a62003ccc676b4da6fda
cliu268 <cliu268@gmail.com>
2020-07-12 10:27:18 -0400

    calculator styles

--- cal.js
+++ cal.js
@@ -1,5 +1,5 @@
 let currentValue = 0;
 function hit(clicked_id) {
-    currentValue = currentValue * 10 + parseInt(clicked_id);
-    document.getElementById("Output").innerHTML = currentValue;
+    currentValue = currentValue * 10 + parseInt(clicked_id);    
+    document.getElementById("Output").innerHTML = currentValue.toLocaleString();
 }
